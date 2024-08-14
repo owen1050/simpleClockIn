486a4075de05e2c8f48bb8a64bea138f3d9dd8a2
Sisters1st <89617423+Sisters1st@users.noreply.github.com>
2024-08-14 10:48:01 -0400

    categories and varsity check works

--- src/static/hours.js
+++ src/static/hours.js
@@ -4,6 +4,7 @@ url = "http://localhost:5000"
 var id = new URLSearchParams(window.location.search).get('id')
 let signIns = getUserTimes(id)
 let categories = getAllCategories()
+let totalHours = 0;
 
 var catToColMap = new Map();
 catToColMap.set(0,8);
@@ -17,6 +18,7 @@ catToColMap.set(9,7);
 
 var catTable = document.getElementById("categoryTableID")
 cols = catTable.rows[0].cells.length
+var statusText = document.getElementById("statusText")
 
 //row numbers
 newRow = catTable.insertRow(catTable.rows.length)
@@ -24,25 +26,116 @@ for(let i = 0; i < cols; i++){
 	newRow.insertCell(i).innerHTML = i;
 }
 
+var busStatus = []
+var buildStatus = []
+
 for(let row = 0; row < categories.length; row++){
     newRow = catTable.insertRow(catTable.rows.length);
     for(let col = 0; col < 11; col++){
+        if(col == 0){
+            newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)];
+        }
         if(col == 1){
             newRow.insertCell(col).innerHTML = Math.round(getHoursInCategory(categories[row][0])/3600 * 100) / 100;
-        } else if(col == 6){
-            newRow.insertCell(col).innerHTML = "nTc";
-        } else if(col == 10){
-            newRow.insertCell(col).innerHTML = "nTc";
-        }else{
-            if(col == 2 || col == 0){
-                newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)];
-            } else {
-                newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)] * categories[row][catToColMap.get(2)];
+            var userHours = Number(Math.round(getHoursInCategory(categories[row][0])/3600 * 100) / 100)
+            totalHours = Number(totalHours) + Number(userHours)
+        }
+        if(col == 2){
+            newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)];
+        }
+        if(col == 3){
+            newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)] * categories[row][catToColMap.get(2)];
+            var buildVar = Number(categories[row][catToColMap.get(col)])* categories[row][catToColMap.get(2)];
+        }
+        if(col == 4){
+            newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)] * categories[row][catToColMap.get(2)];
+            var buildJV = Number(categories[row][catToColMap.get(col)])* categories[row][catToColMap.get(2)];
+        }
+        if(col == 5){
+            newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)] * categories[row][catToColMap.get(2)];
+            var buildP = Number(categories[row][catToColMap.get(col)])* categories[row][catToColMap.get(2)];
+        }
+        if(col == 6){
+            str = "None"
+            if(userHours > buildP){
+                str = "P"
             }
+            if(userHours > buildJV){
+                str = "JV"
+            }
+            if(userHours > buildVar){
+                str = "V"
+            }
+            if(buildVar == 0){
+                str = "n/a"
+            }
+            newRow.insertCell(col).innerHTML = str; //calulate build status
+            buildStatus.push(str)
+        }
+        if(col == 7){
+            var busVar = Number(categories[row][catToColMap.get(col)]) * categories[row][catToColMap.get(2)]
+            newRow.insertCell(col).innerHTML = busVar;
             
         }
+        if(col == 8){
+            newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)] * categories[row][catToColMap.get(2)];
+            var busJV = Number(categories[row][catToColMap.get(col)]) * categories[row][catToColMap.get(2)]
+        }
+        if(col == 9){
+            newRow.insertCell(col).innerHTML = categories[row][catToColMap.get(col)] * categories[row][catToColMap.get(2)];
+            var busP = Number(categories[row][catToColMap.get(col)]) * categories[row][catToColMap.get(2)]
+        }
+        if(col == 10){
+            str = "None"
+            if(userHours > busP){
+                str = "P"
+            }
+            if(userHours > busJV){
+                str = "JV"
+            }
+            if(userHours > busVar){
+                str = "V"
+            }
+            if(busVar == 0){
+                str = "n/a"
+            }
+            newRow.insertCell(col).innerHTML = str; //calulate build status
+            busStatus.push(str) 
+        }
+        
     }
 }
+
+lowestStatus = "V"
+for(let i = 0; i < busStatus.length; i++){
+    if(lowestStatus == "V" && (busStatus[i] == 'P' || busStatus[i] == 'JV' || busStatus[i] == "None")){
+        lowestStatus = busStatus[i]
+    }
+    if(lowestStatus == "JV" && (busStatus[i] == 'P' || busStatus[i] == "None")){
+        lowestStatus = busStatus[i]
+    }
+    if(lowestStatus == "P" && (busStatus[i] == "None")){
+        lowestStatus = busStatus[i]
+    }
+}
+
+lowestBuildStatus = "V"
+for(let i = 0; i < buildStatus.length; i++){
+    if(lowestBuildStatus == "V" && (buildStatus[i] == 'P' || buildStatus[i] == 'JV' || buildStatus[i] == "None")){
+        lowestBuildStatus = buildStatus[i]
+    }
+    if(lowestBuildStatus == "JV" && (buildStatus[i] == 'P' || buildStatus[i] == "None")){
+        lowestBuildStatus = buildStatus[i]
+    }
+    if(lowestBuildStatus == "P" && (buildStatus[i] == "None")){
+        lowestBuildStatus = buildStatus[i]
+    }
+}
+
+statusText.innerHTML = "Total Hours: " + totalHours + ". Build Varsity Status: " + lowestBuildStatus + ". Business Varsity Status: " + lowestStatus + ".    "
+
+console.log("BusStatus:" + lowestStatus)
+console.log("BuildStatus:" + lowestBuildStatus)
 
 
 function getHoursInCategory(catId){
